20eeee5e479d841dec93b1f7dd6d527bcfee8300
mencorium <mwalyambijoel@gmail.com>
2023-08-30 07:32:57 +0300

    first commit

---
+++ application.php
@@ -0,0 +1,139 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>lasuperior</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/swap1.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/swap-google.css">
+    <link rel="stylesheet" href="./css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/footer-ultimate.css">
+    <link rel="stylesheet" href="./css/Navbar-Right-Links-Dark-icons.css">
+    <link rel="stylesheet" href="./css/Sakae-Simple-Section.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="card shadow-sm">
+        <div class="card-body">
+            <section id="back-ground" style="background: url(&quot;./images/whitePeachBack.jpg&quot;) center / cover;/*height: 100vh;*/">
+                <nav class="navbar navbar-light navbar-expand-md py-3" id="joel">
+                    <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="text-start text-" style="--bs-body-color: #f96f0b;color: #22dd83;">La Superior Restaurant</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-3" style="--bs-warning: #22dd83;--bs-warning-rgb: 34,221,131;"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon" style="--bs-danger: #22e7a0;--bs-danger-rgb: 34,231,160;"></span></button>
+                        <div class="collapse navbar-collapse" id="navcol-3">
+                            <ul class="navbar-nav mx-auto">
+                                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">About us</a><a class="nav-link" href="#">Contact us</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Log in</a><a class="nav-link" href="#">sign in</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div class="container text-center flex-row">
+                    <div class="d-print-none d-xxl-table text-center" id="div1"><img class="rounded-circle img-fluid shadow-lg" src="./images/logo4.png" style="width: 200px;background: var(--bs-gray-100);text-shadow: 0px 0px;"></div>
+                    <div class="text-center">
+                        <nav class="navbar navbar-light navbar-expand-md justify-content-center align-items-center align-content-center" id="nav-bar" style="width: 700px;/*margin-left: 30px;*/background: #abb1b5;text-align: center;opacity: 0.80;">
+                            <div class="container-fluid"><a class="navbar-brand" href="#"></a><img src="./images/logo4.png" style="width: 70px;"><button data-bs-toggle="collapse" data-bs-target="#navcol-2" class="navbar-toggler"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                                <div class="collapse navbar-collapse" id="navcol-1" style="font-size: 28px;font-weight: bold;text-align: center;">
+                                    <ul class="navbar-nav justify-content-center align-items-center align-content-center">
+                                        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">About us</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">Contact us</a></li>
+                                    </ul>
+                                </div>
+                                <div class="collapse navbar-collapse" id="navcol-2" style="font-size: 25px;font-family: Actor, sans-serif;font-weight: bold;text-align: center;color: rgb(154,212,225);">
+                                    <ul class="navbar-nav justify-content-center align-items-center align-content-center">
+                                        <li class="nav-item"><a class="nav-link active" href="#">Log in</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="#">&nbsp;sign&nbsp;</a></li>
+                                        <li class="nav-item"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <div class="container">
+                    <div style="text-align: left;">
+                        <h1 class="text-center alert-heading" id="la-superior" style="/*font-family: Akronim, serif;*/font-size: 50px;text-align: center;/*color: var(--bs-indigo);*/font-style: italic;background: #00000000;">la superrior restaurant</h1>
+                        <h1>Our Food,</h1>
+                        <h1 style="font-family: Adamina, serif;">our Culture,</h1>
+                        <h1 class="blockquote" style="font-family: 'Advent Pro', sans-serif;text-align: left;font-size: 27px;">"laughter is brightest where food is the best..."</h1>
+                </div>
+            </section>
+        </div>
+        <button class="btn btn-primary text-center text-sm-center d-lg-flex justify-content-lg-center align-items-lg-end" type="button" style="color: var(--bs-btn-disabled-color);background: var(--bs-gray-700);width: 200px;margin-top: 50px;position: absolute;overflow: auto;transform: scale(1.30);">Come And Join Us</button>
+    </div>
+    <div style="margin: 50px;">
+        <div class="container sombra borderedondo">
+            <div class="row">
+                <div class="col-md-4"><i class="fa fa-play-circle-o border-dark" style="font-size: 35px;margin: 10px;"></i>
+                    <div class="imgRedonda"><img class="imgpeque" src="/desk.jpg"></div>
+                    <h1 class="text-center">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus lacus ac viverra fermentum. Aliquam erat volutpat. Etiam volutpat aliquet sapien, at sagittis justo iaculis in. Cras blandit justo risus, quis fermentum odio ullamcorper eu.&nbsp;</p>
+                    <div class="row">
+                        <div class="col text-end"><button class="btn btn-primary" type="button" style="margin: 10px;">More...</button></div>
+                    </div>
+                </div>
+                <div class="col-md-4"><i class="fa fa-play-circle-o border-dark" style="font-size: 35px;margin: 10px;"></i>
+                    <div class="imgRedonda"><img class="imgpeque" src="/desk.jpg"></div>
+                    <h1 class="text-center">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus lacus ac viverra fermentum. Aliquam erat volutpat. Etiam volutpat aliquet sapien, at sagittis justo iaculis in. Cras blandit justo risus, quis fermentum odio ullamcorper eu.&nbsp;</p>
+                    <div class="row">
+                        <div class="col text-end"><button class="btn btn-primary" type="button" style="margin: 10px;">More...</button></div>
+                    </div>
+                </div>
+                <div class="col-md-4"><i class="fa fa-play-circle-o border-dark" style="font-size: 35px;margin: 10px;"></i>
+                    <div class="imgRedonda"><img class="imgpeque" src="/desk.jpg"></div>
+                    <h1 class="text-center">Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus lacus ac viverra fermentum. Aliquam erat volutpat. Etiam volutpat aliquet sapien, at sagittis justo iaculis in. Cras blandit justo risus, quis fermentum odio ullamcorper eu.&nbsp;</p>
+                    <div class="row">
+                        <div class="col text-end"><button class="btn btn-primary" type="button" style="margin: 10px;">More...</button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-clean">
+        <footer>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3 item social"><img class="img-fluid footer-logo" src="./images/logo4.png">
+                        <p class="text-center footer-logo-text">Tastes<br>like a little bit of heaven in a pan. A real piece of an art! Bring the sparkle<br>in your eye out to dine! Fine dining unlike anything else you’ve ever tasted.<br>Stirring your imagination one satisfying meal at a time. Let’s go out tonight!<br>We know how to make you happy! We make every moment count!<br><br></p>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item-srv">
+                        <h3>Informations</h3>
+                        <ul>
+                            <li><a href="contact.html">Contact us</a></li>
+                            <li><a href="https://ultimatecomputers.ro/shop/index.php?route=information/information&amp;information_id=6"></a></li>
+                            <li><a href="https://ultimatecomputers.ro/shop/index.php?route=information/information&amp;information_id=3">Privacy and policy</a></li>
+                            <li><a href="https://ultimatecomputers.ro/shop/index.php?route=information/information&amp;information_id=5">Terms and condition</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Despre noi</h3>
+                        <ul>
+                            <li class="despre-noi"><i class="fa fa-home despre-noi"></i>&nbsp;street address:</li>
+                            <li class="despre-noi"><i class="fa fa-envelope despre-noi"></i>&nbsp;Email: lasuperior@gmail.com</li>
+                            <li class="despre-noi"><i class="fa fa-phone despre-noi"></i>&nbsp;Telephone: 0766 4854 60</li>
+                        </ul>
+                        <ul></ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script src="./js/bootstrap.min.js"></script>
+    <script id="bs-live-reload" data-sseport="50790" data-lastchange="1693362352290" src="./js/liverload.js"></script>
+</body>
+
+</html>
